d1f7f0115073859f74d8c5fd58fe435f00c35b24
NewVersionOfAberta <natalia101800@gmail.com>
2020-10-06 23:06:55 +0300

    add generators

---
+++ FakerConfig.cs
@@ -0,0 +1,23 @@
+﻿using FakerGeneratorInterfases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerLab
+{
+    class FakerConfig
+    {
+        public void Add<ClassType, ResultType, GeneratorType>(Expression<Func<ClassType, ResultType>> expression){
+            if (expression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                MemberExpression memberExpression = (MemberExpression)expression.Body;
+
+                var generator = (ISimpleGenerator)Activator.CreateInstance(typeof(GeneratorType));
+                Faker.customGenerators.Add((memberExpression.Member.Name, typeof(ClassType)), generator);
+            }
+        }
+    }
+}
